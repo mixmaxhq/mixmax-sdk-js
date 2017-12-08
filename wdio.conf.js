6a21a90a3366365e1f16a6560da68da3ba1a21d8
Eli Skeggs <eli@mixmax.com>
2017-12-08 17:36:49 -0500

    Expand set of environments

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,14 +1,61 @@
+const detonate = require('detonate-object');
+
 exports.config = {
   specs: [
     './specs/**/*.js'
   ],
   maxInstances: 1,
-  capabilities: [{
-    browserName: 'chrome',
-    version: 43,
-    name: 'Chrome Selenium tests',
-    build: process.env.TRAVIS_BUILD_NUMBER
-  }],
+  capabilities: Array.from(detonate({
+    $each: [{
+      browserName: 'chrome',
+      // Support the last twenty chrome versions, skipping every 5.
+      version: {$each: latestVersions(0, 20, 5)}
+    }, {
+      browserName: 'firefox',
+      // Support the last twenty firefox version, skipping every 5.
+      version: {$each: latestVersions(0, 20, 5)}
+    }, {
+      browserName: 'safari',
+      // Custom supported version/platform combinations.
+      $each: [{
+        version: '7.0',
+        platform: 'OS X 10.9'
+      }, {
+        version: '8.0',
+        platform: 'OS X 10.10'
+      }, {
+        version: '9.0',
+        platform: 'OS X 10.11'
+      }, {
+        version: '10.0',
+        platform: 'OS X 10.11'
+      }, {
+        version: '11.0',
+        platform: 'macOS 10.12'
+      }]
+    }, {
+      browserName: 'internet explorer',
+      platform: 'Windows 7',
+      version: {$each: ['8.0', '9.0', '10.0']}
+    }, {
+      platform: 'Windows 10',
+      $each: [{
+        browserName: 'internet explorer',
+        version: '11.103'
+      }, {
+        browserName: 'MicrosoftEdge',
+        version: {$each: ['13.14393', '15.15063']}
+      }]
+    }],
+    platform: {$each: [
+      'OS X 10.9',
+      'macOS 10.12',
+      'Windows 10',
+      'Windows 8.1'
+    ]},
+    build: process.env.TRAVIS_BUILD_NUMBER,
+    name: 'SDK Tests'
+  })),
   sync: true,
   logLevel: 'error',
   coloredLogs: true,
@@ -43,4 +90,14 @@ if (isTravis) {
     services: ['sauce'],
     sauceConnect: true
   });
+}
+
+function* range(start, end, skip) {
+  for (let i = start; i < end; i += skip) {
+    yield i;
+  }
+}
+
+function latestVersions(start, end, skip) {
+  return Array.from(range(start, end, skip)).map((value) => value ? `latest-${value}` : 'latest');
 }
